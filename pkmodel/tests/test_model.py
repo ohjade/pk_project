5a164e8c9da2d65f75b1396606dde79a0bb5846f
ohjade <120578702+ohjade@users.noreply.github.com>
2023-10-21 00:43:58 +0100

    Update test_model.py
    testing dose function and solve function

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -1,17 +1,17 @@
 import unittest
-import pkmodel as pk
+from pkmodel.model import Model
 
 
-class ModelTest(unittest.TestCase):
-    """
-    Tests the :class:`Model` class.
-    """
-    def test_create(self):
-        """
-        Tests Model creation.
-        """
-        model = pk.Model()
-        self.assertEqual(model.value, 42)
+class TestModel(unittest.TestCase):
+
+    def test_dose_function(self):
+        test_model = Model("test_model", 1, "intravenous", 1, 1, 1, 1, 1, 1)
+        result = test_model.dose(0, 10)
+        self.assertEqual(result, 10)
+
+    def test_solve_function(self):
+        test_model = Model("test_model", 1, "intravenous", 1, 1, 1, 1, 1, 1)
+        sol = test_model.solve()
 
 if __name__ == '__main__':
     unittest.main()
